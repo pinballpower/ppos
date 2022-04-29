b5e9a9c33c871d191c634d33c96a09f23ead06ec
Pinball Power <info@pinball-power.com>
2022-04-29 18:07:33 +0000

    Integrated VC4 config directly into configtxt package

--- brext/package/configtxt/configtxt.mk
+++ brext/package/configtxt/configtxt.mk
@@ -10,6 +10,15 @@ define CONFIGTXT_INSTALL_TARGET_CMDS
         echo "# Enable I2C and SPI" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 	echo "dtparam=i2c=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 	echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+
+        # Add VC4 overlay
+        sed -i s/.*vc4-*kms-v3d.*//g $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "dtoverlay=vc4-fkms-v3d,audio=off" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+
+        # Make sure it gets registered in systemd
+        mkdir -p $(TARGET_DIR)/usr/lib/udev/rules.d
+        cp $(BR2_EXTERNAL_PPOS_PATH)/package/configtxt/60-drm.rules \
+		$(TARGET_DIR)/usr/lib/udev/rules.d/60-drm.rules
 endef
 
 

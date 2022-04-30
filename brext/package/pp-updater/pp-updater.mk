fa59a7e91b7651bfbe26d7ec5d1625f62848ed19
Pinball Power <info@pinball-power.com>
2022-04-30 07:13:34 +0000

    Remove unused stuff

--- brext/package/pp-updater/pp-updater.mk
+++ brext/package/pp-updater/pp-updater.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PP_UPDATER_DEPENDENCIES = systemd copy-overlays
+PP_UPDATER_DEPENDENCIES = systemd
 
 define PP_UPDATER_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_PPOS_PATH)/package/pp-updater/extract-update \
@@ -19,18 +19,16 @@ define PP_UPDATER_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/opt/ppos/bin
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_PPOS_PATH)/package/pp-updater/restore-config \
                 $(TARGET_DIR)/opt/ppos/bin
-        $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_PPOS_PATH)/VERSION \
+        $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_PPOS_PATH)/../VERSION \
                 $(TARGET_DIR)/etc/ppos.version
-        $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_PPOS_PATH)/PIVERSION \
-                $(TARGET_DIR)/etc/raspberrypi.version
         $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_PPOS_PATH)/package/pp-updater/config-files \
                 $(TARGET_DIR)/opt/ppos/etc/config-files
         $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_PPOS_PATH)/package/pp-updater/services \
                 $(TARGET_DIR)/opt/ppos/etc/services
 	echo "Installing kernel"
-        $(INSTALL) -D -m 0644 $(BUILD_DIR)/linux-custom/arch/arm/boot/zImage $(TARGET_DIR)/usr/lib/firmware/rpi
+        $(INSTALL) -D -m 0644 $(BUILD_DIR)/linux-custom/arch/arm64/boot/Image.gz $(TARGET_DIR)/usr/lib/firmware/rpi
 	echo "Installing updater"
-	$(INSTALL) -D -m 755 $(BR2_EXTERNAL_PPOS_PATH)/../scripts/updater.sh $(TARGET_DIR)/tmp/updater.sh
+	$(INSTALL) -D -m 755 $(BR2_EXTERNAL_PPOS_PATH)/updater.sh $(TARGET_DIR)/tmp/updater.sh
 
 endef
 
